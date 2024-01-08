5a7645adf78f921601ba403e3d19b1c646b21fa9
s1f999999999 <85667263+s1f999999999@users.noreply.github.com>
2024-01-09 01:07:56 +0900

    cccc
    cccc

--- kadai.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toggleButton = document.getElementById('toggle');
-    var menu = document.getElementById('menu');
-
-    toggleButton.addEventListener('click', function() {
-        menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
-    });
-});
